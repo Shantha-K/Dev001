73543d17ac4845f58bdbe0cb0a110766359c0078
shantha-123 <k36440670@gmail.com>
2025-06-23 18:40:16 +0530

    final commit

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,14 @@
 module.exports = {
     url: 'mongodb://localhost:27017/crud-node-express'
 }
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function connectToDB() {
+  await client.connect();
+  console.log("Connected to MongoDB Atlas");
+  return client.db();
+}
+
+module.exports = connectToDB;
