ca0c9e4e309770f61b968e612836e665010c3898
shantha-123 <k36440670@gmail.com>
2025-07-03 16:42:26 +0530

    dhasud

--- config/database.config.js
+++ config/database.config.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//     // url:  "mongodb://final:shantha1234@13.204.58.168:27017/mydatabase"
-// }
-
 module.exports = {
-    // url:  "mongodb://final:shantha@35.154.247.128:27017/admin"
-    url: "mongodb://mongodb:27017/mydatabase"
+  url: process.env.MONGO_URL || "mongodb://admin:admin123@localhost:27017/mydatabase?authSource=admin"
 }
